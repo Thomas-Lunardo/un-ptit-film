43baf5914e1197cf94a8d2cbd6d6b4ab26149ab6
THOMAS <thomaslunardo@gmail.com>
2024-05-31 23:38:23 +0200

    configuration of webpack encore and bootstrap

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,10 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/app.scss';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+// import logoPath from '../images/';
+
+// let html = `<img src="${logoPath}" alt="ACME logo">`;
